369a6cd98e0586852310c98f1d4919abe5116da5
dmswls123 <dmswls203@naver.com>
2024-05-11 17:54:09 +0900

    (2024.05.11) 플레이어 애니메이션, 타일맵 구현
    2일차
    
    유니티 전용 주석 => /// 쓰면 변수나 함수 갖다댔을때 바로 설명보임
    
    [C# 클래스 관련]
    public: 클래스 외부에서도 사용 가능, 유니티에서는 인스펙터창에 변수가 노출이 됨, 즉 접근이 가능함
    private: 그 클래스 안에서만 사용 가능
    protected
    
    이름 공통으로 바꾸는 기능: Ctrl+R+R
    
    플레이어 기본 셋팅 후 애니메이션 들어감.
    1. 방향키로 방향 보는 것
    2. 애니메이션(Idle, Fall, Hit, Run, Jump)
    3. 떨어졌을 때 다시 처음장소로 오는 것 추가
    4. tile palatte 이용해서 저장되어 있는 그래픽 들고와서 추가함 => rigidbody2D, tilemap collider 2D, Composite Collider 2D 넣어서 설정함
    5. 메인카메라를 플레이어 캐릭터에 놓고 움직이게 함
    
    * 스크립트 창에서 쓴 변수가 inpector창에 보이게 하려면 private말고 public 써야함
    
    애니메이터에서 parameters 만들면 스크립트 창에도 넣어주면 조건생성 완성됨

--- Platfromer Project/Assets/Scripts/PlayerController.cs
+++ Platfromer Project/Assets/Scripts/PlayerController.cs
@@ -13,8 +13,8 @@ public class PlayerController : MonoBehaviour
     // 첫 프레임이 불러지기전에 (한번)시작한다.(위에 줄 번역) - 셋팅느낌, 젤 처음에만
 
     // 움직이기 위해서 필요한 것: 속도, 방향
-    [Header("이동")]
-    public float moveSpeed=5f;  // 캐릭터의 이동 속도 변수
+    [Header("플레이어 입력")]
+    public float moveSpeed = 5f;  // 캐릭터의 이동 속도 변수
     public float JumpForce = 5f; // 캐릭터 점프
     private float moveInput; // 플레이어의 방향 및 인풋 데이터 저장 변수
 
@@ -26,33 +26,127 @@ public class PlayerController : MonoBehaviour
     public float groundDistance = 2f;
     public LayerMask groundLayer;
 
+    [Header("Flip")]
+    public SpriteRenderer spriteRenderer;
+    private bool facingRight = false;
+    private int facingDirection = 1;
+
+    public Animator animator;
+    private bool isMove;
+
 
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
-        Debug.Log("Hello Unity");
         // 현재 내 위치 <= 새로운 x,y 저장하는 데이터 타입(현재 x좌표, 10 y좌표)
         //transform.position = new Vector2(transform.position.x, 10);
 
+        InitializePlayerStatus();
+
+
+    }
+
+    void InitializePlayerStatus() // 최초 설정으로 변경해준다는 설정
+    {
+
         // 현재 내 위치를 startTransform으로 변경
         transform.position = startTransform.position;
+        // 땅에 떨어지는 속도 설정
+        rigidbody2D.velocity = Vector2.zero;
+        // 왼쪽 보고 떨어져도 다시 처음 설정할때 오른쪽 바라보게
+        facingRight = true;
+        spriteRenderer.flipX = false;
+        // 위에를 다 함으로써 기본 세팅값으로 돌아옴
     }
 
     // Update is called once per frame
     // 1프레임마다 한번 호출된다. 1프레임마다 호출된다는 뜻인 듯(반복 실행)
     void Update()
     {
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundDistance, groundLayer); // 광선을 쏴서 ~머시기
-        
-        // 플레이어의 입력 값을 받아와야 함
-        moveInput = Input.GetAxis("Horizontal"); // 수평에 있는 축 데이터 값을 받아오는 것
-        rigidbody2D.velocity = new Vector2(moveSpeed * moveInput, rigidbody2D.velocity.y); // 방향키로 움직이는 것
+        HandleAnimation();
+        CollisionCheck();
+        HandleInput();
+        HandleFlip();
+        Move();
+        FallDownCheck();
+    }
 
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded==true) // 키가 눌러져있고, 땅위에 있을 때
+    private void FallDownCheck()
+    {
+        // y의 높이가 특정 지점보다 낮을 때 낙사한 것으로 간주한다. => 충돌 체크 대체
+        if (transform.position.y < -11)
+        {
+            InitializePlayerStatus();
+        }
+    }
+
+    private void HandleAnimation()
+    {
+        // rigidbody.velocity : 현재 rigidbody 속도를 알려주는 것 속도가 0이면 움직이지 않는 상태, 0이 아니면 움직이고 있는 상태
+        isMove = rigidbody2D.velocity.x != 0; 
+        animator.SetBool("isMove", isMove);
+        animator.SetBool("isGrounded", isGrounded);
+        // SetFloat 함수에 의해서 y최대일 때 1로 변환..  y 최소일 때 -1로 변환
+        // 점프 키를 누르면 순간적으로 y 높이 증가, 중력에 의해서 점점 y의 속도가 -까지 내려감
+        animator.SetFloat("yVelocity", rigidbody2D.velocity.y);  
+    }
+
+    /// <summary>
+    /// 점프를 할 때 땅인지 아닌지 체크하는 기능 -> Collider Check
+    /// </summary>
+    private void CollisionCheck()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundDistance, groundLayer); // 광선을 쏴서 ~머시기
+    }
+    /// <summary>
+    ///  // 플레이어의 입력 값을 받아와야 함. a,d 키보드 좌 우 키를 눌렀을 때 -1 ~ 1 반환하는 클래스
+
+    /// </summary>
+    private void HandleInput()
+    {
+        moveInput = Input.GetAxis("Horizontal"); // 수평에 있는 축 데이터 값을 받아오는 것
+       
+        JumpButton();
+    }
+
+    private void HandleFlip()
+    {
+        // 오른쪽 방향으로 바라보고 있을 때
+        if(facingRight && moveInput < 0)
+        {
+            Flip();
+        }
+        // 왼쪽 방향으로 바라보고 있을 때
+        else if(!facingRight && moveInput > 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        facingDirection = facingDirection * - 1;
+        facingRight = !facingRight;
+        spriteRenderer.flipX = !facingRight;
+    }
+
+    private void Move()
+    {
+        rigidbody2D.velocity = new Vector2(moveSpeed * moveInput, rigidbody2D.velocity.y); // 방향키로 움직이는 것
+    }
+
+    private void JumpButton()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true) // 키가 눌러져있고, 땅위에 있을 때
         {
             // 점프: Y position _ rigidbody Y velocity를 점프 파워만큼 올려주면 됨
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, JumpForce);
+            Jump();
         }
+    }
+
+    private void Jump()
+    {
+        rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, JumpForce);
     }
 
     private void OnDrawGizmos()
